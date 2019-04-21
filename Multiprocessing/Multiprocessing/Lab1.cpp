dda6c3524b017f7f00c1fb5c5ff54059b80e6e44
Rinuys <dlghdtjr1492@naver.com>
2019-04-21 12:49:30 +0900

    데탑 테스트

--- Multiprocessing/Multiprocessing/Lab1.cpp
+++ Multiprocessing/Multiprocessing/Lab1.cpp
@@ -1,23 +1,30 @@
 #include<iostream>
 #include<ctime>
-#include<Windows.h>
 #include<cstdio>
-#include<ctime>
-#define ITERNUM 6250000
+#include<Windows.h>
+
+#define _ITERNUM 166666667LL
+#define THREADNUM 3
+#define AVGITER 10
+
 using namespace std;
 
-int acc = 0;
+long long int acc = 0;
+
 DWORD addr;
 HANDLE hd_th1, hd_th2, hd_th3, hd_th4,
-	   hd_th5, hd_th6, hd_th7, hd_th8;
+	   hd_th5, hd_th6, hd_th7, hd_th8,
+	   hd_th9, hd_th10, hd_th11, hd_th12;
 
 DWORD WINAPI Bignumber(LPVOID lpVoid) {
-	for (int i = 0; i < ITERNUM; i++) acc += 2;
-	return 0;
+	SetThreadAffinityMask(GetCurrentThread(), (int)lpVoid);
+	//long long int ITERNUM = _ITERNUM / THREADNUM;
+	for (long long int i = 0; i < _ITERNUM; i++) acc += 2;
+	return 0;;
 }
 
-void createThread(HANDLE &hd_th) {
-	hd_th = CreateThread(NULL, 0, Bignumber, NULL, 0, &addr);	
+void createThread(HANDLE &hd_th, int affinity) {
+	hd_th = CreateThread(NULL, 0, Bignumber, (void*)affinity, 0, &addr);
 }
 
 void waitThread(HANDLE &hd_th) {
@@ -28,31 +35,39 @@ void waitThread(HANDLE &hd_th) {
 int main() {
 	clock_t avg = 0;
 	long long int bignum_avg = 0;
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < AVGITER; i++) {
 		acc = 0;
-		clock_t start = clock();
-		createThread(hd_th1);
-		createThread(hd_th2);
-		createThread(hd_th3);
-		createThread(hd_th4);
-		createThread(hd_th5);
-		createThread(hd_th6);
-		createThread(hd_th7);
-		createThread(hd_th8);
+		DWORD start = GetTickCount();
+		createThread(hd_th1, 1);
+		createThread(hd_th2, 4);
+		createThread(hd_th3, 16);
+		/*createThread(hd_th4, 64);
+		createThread(hd_th5, 256);
+		createThread(hd_th6, 1024);
+		createThread(hd_th7, 2);
+		createThread(hd_th8, 8);
+		createThread(hd_th9, 32);
+		createThread(hd_th10, 128);
+		createThread(hd_th11, 512);
+		createThread(hd_th12, 2048);*/
 		waitThread(hd_th1);
 		waitThread(hd_th2);
 		waitThread(hd_th3);
-		waitThread(hd_th4);
+		/*waitThread(hd_th4);
 		waitThread(hd_th5);
 		waitThread(hd_th6);
 		waitThread(hd_th7);
 		waitThread(hd_th8);
-		clock_t end = clock();
-		avg += (end - start);
+		waitThread(hd_th9);
+		waitThread(hd_th10);
+		waitThread(hd_th11);
+		waitThread(hd_th12);*/
+		DWORD end = GetTickCount();
+		avg += ((int)end - (int)start);
 		bignum_avg += acc;
-		if ((i + 1) % 10 == 0) printf("%d%% ¿Ï·á\n",i+1);
+		printf("%d%% ¿Ï·á\n",(i+1)*10);
 	}
-	avg /= 100;
-	bignum_avg /= 100;
+	avg /= AVGITER;
+	bignum_avg /= AVGITER;
 	printf("Æò±ÕBignum : %lld, Æò±Õ½Ã°£ : %dms\n",bignum_avg,avg);
 }
