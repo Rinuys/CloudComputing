9af963038c4a9f47036097de9c57d37c3cfd1c86
Rinuys <dlghdtjr1492@naver.com>
2019-05-04 13:49:00 +0900

    a

--- Multiprocessing/Multiprocessing/Lab1.cpp
+++ Multiprocessing/Multiprocessing/Lab1.cpp
@@ -3,7 +3,7 @@
 #include<cstdio>
 #include<Windows.h>
 
-#define _ITERNUM 166666667LL
+#define _ITERNUM 41666667LL
 #define THREADNUM 3
 #define AVGITER 10
 
@@ -19,7 +19,9 @@ HANDLE hd_th1, hd_th2, hd_th3, hd_th4,
 DWORD WINAPI Bignumber(LPVOID lpVoid) {
 	SetThreadAffinityMask(GetCurrentThread(), (int)lpVoid);
 	//long long int ITERNUM = _ITERNUM / THREADNUM;
-	for (long long int i = 0; i < _ITERNUM; i++) acc += 2;
+	long long int temp = 0;
+	for (long long int i = 0; i < _ITERNUM; i++) temp += 2;
+	acc += temp;
 	return 0;;
 }
 
@@ -41,7 +43,7 @@ int main() {
 		createThread(hd_th1, 1);
 		createThread(hd_th2, 4);
 		createThread(hd_th3, 16);
-		/*createThread(hd_th4, 64);
+		createThread(hd_th4, 64);
 		createThread(hd_th5, 256);
 		createThread(hd_th6, 1024);
 		createThread(hd_th7, 2);
@@ -49,11 +51,11 @@ int main() {
 		createThread(hd_th9, 32);
 		createThread(hd_th10, 128);
 		createThread(hd_th11, 512);
-		createThread(hd_th12, 2048);*/
+		createThread(hd_th12, 2048);
 		waitThread(hd_th1);
 		waitThread(hd_th2);
 		waitThread(hd_th3);
-		/*waitThread(hd_th4);
+		waitThread(hd_th4);
 		waitThread(hd_th5);
 		waitThread(hd_th6);
 		waitThread(hd_th7);
@@ -61,7 +63,7 @@ int main() {
 		waitThread(hd_th9);
 		waitThread(hd_th10);
 		waitThread(hd_th11);
-		waitThread(hd_th12);*/
+		waitThread(hd_th12);
 		DWORD end = GetTickCount();
 		avg += ((int)end - (int)start);
 		bignum_avg += acc;
