f9e953750cc1ebc2112602f68cdfa1801685dc77
Rinuys <dlghdtjr1492@naver.com>
2019-05-12 19:02:17 +0900

    STL Sort

--- MultiSorting/MultiSorting/MultiSorting/Source.cpp
+++ MultiSorting/MultiSorting/MultiSorting/Source.cpp
@@ -1,8 +1,43 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
+#include<ctime>
+#include<cstdlib>
+
+#define NUMBEROFSTRINGS 10000						// 문자열의 개수
+#define NUMBEROFCHARS 10							// 문자의 개수
 
 using namespace std;
 
+string st[NUMBEROFSTRINGS];							// 정렬할 문자열 배열
+
+void createStrings() {								// 문자열 생성
+	for (int i = 0; i < NUMBEROFSTRINGS; i++) {
+		string temp="";
+		for (int j = 0; j < NUMBEROFCHARS; j++) {
+			int r = rand() % 36;					// 숫자 + 문자 = 36자
+			char c;
+			if (r < 10) c = r + '0';				// 숫자(10자)
+			else		c = r - 10 + 'A';			// 문자(26자)
+			temp.push_back(c);
+		}
+		st[i] = temp;
+	}
+}
+
+void printStrings(int num) {						// 문자열 배열 출력
+	for (int i = 0; i < num; i++) 
+		cout << st[i] << endl;
+}
+
 int main() {
+	srand(time(0));
+	createStrings();
+
+	printStrings(5);
+	sort(st, &st[NUMBEROFSTRINGS]);
 	cout << "HelloWorld!" << endl;
+	printStrings(5);
+
 	return 0;
 }
