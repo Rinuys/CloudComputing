6a9c67c4df02ed5313508ccdf07780c50dc613ff
Rinuys <dlghdtjr1492@naver.com>
2019-05-31 19:31:14 +0900

    수정중

--- MultiSorting/MultiSorting/MultiSorting/Source.cpp
+++ MultiSorting/MultiSorting/MultiSorting/Source.cpp
@@ -4,12 +4,13 @@
 #include<ctime>
 #include<cstdlib>
 
-#define NUMBEROFSTRINGS 10000						// 문자열의 개수
+#define NUMBEROFSTRINGS 100000000					// 문자열의 개수
 #define NUMBEROFCHARS 10							// 문자의 개수
 
 using namespace std;
 
-string st[NUMBEROFSTRINGS];							// 정렬할 문자열 배열
+string *st = new string[NUMBEROFSTRINGS];							// 정렬할 문자열 배열
+char inputfile[20] = "input.txt";
 
 void createStrings() {								// 문자열 생성
 	for (int i = 0; i < NUMBEROFSTRINGS; i++) {
@@ -25,19 +26,44 @@ void createStrings() {								// 문자열 생성
 	}
 }
 
+void saveStrings() {
+	FILE *fp = fopen(inputfile,"w+");
+	for (int j = 0; j < NUMBEROFSTRINGS / 10 ; j++) {
+		fprintf(fp, "%s", st[j*10]);
+		for (int i = 1; i < 10; i++) {
+			fprintf(fp, " %s", st[j * 10 + i]);
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+}
+
+void loadStrings() {
+	FILE *fp = fopen(inputfile, "r");
+	for (int i = 0; i < NUMBEROFSTRINGS; i++) 
+		fscanf(fp, "%s", st[i]);
+	
+	fclose(fp);
+}
+
 void printStrings(int num) {						// 문자열 배열 출력
 	for (int i = 0; i < num; i++) 
 		cout << st[i] << endl;
 }
 
 int main() {
-	srand(time(0));
+	/*srand(time(0));
 	createStrings();
+	saveStrings();*/
+
+	loadStrings();
 
 	printStrings(5);
 	sort(st, &st[NUMBEROFSTRINGS]);
 	cout << "HelloWorld!" << endl;
 	printStrings(5);
+
+	getchar();
 
 	return 0;
 }
