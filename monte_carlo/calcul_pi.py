60d3a77c601febfa9baa55b97178a4f692e5d01a
Rinuys <dlghdtjr1492@naver.com>
2019-06-03 11:34:22 +0900

    c++버전

--- monte_carlo/calcul_pi.py
+++ monte_carlo/calcul_pi.py
@@ -1,11 +1,13 @@
 import random
+import matplotlib.pyplot as plt
 
 # 몬테카를로 계산을 반복할 횟수
-loop = 10**7
+loop = 30000000
 
 
 inside=0
 for _ in range(loop):
     a,b=random.random(), random.random()
     inside += 1 if ( (a*a) + (b*b) ) < 1.0 else 0
+
 print( (inside*4.0)/loop )
